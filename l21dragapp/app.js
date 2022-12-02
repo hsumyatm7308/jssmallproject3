e0ccbe54117d34cf5aa212056715a423b634c79d
EXY <107622230+hsumyatm7308@users.noreply.github.com>
2022-12-02 09:13:04 +0630

    12/2

--- l21dragapp/app.js
+++ l21dragapp/app.js
@@ -1,64 +1,96 @@
 const getbox = document.querySelector('.box');
+const getbtns = document.querySelector('.btns')
+const getboxtitle = document.getElementById('boxtitle');
+
+getbox.addEventListener('click',function(){
+    // getbtns.classList.toggle('show');
+    getbtns.classList.add('show');
+
+    console.log();
+    smallmenu();
+});
+
+getbox.addEventListener('dblclick',function(){
+    getbtns.classList.remove('show');
+})
 
 dragme(getbox);
+
 function dragme(getele){
-    console.log(getele);
+    // console.log(getele);
 
     var getcx,getcy,setcx,setcy;
 
-    getele.onmousedown = getmousedown
+    //design1
+    // getele.onmousedown = getmousedown;
+
+    //design2 
+    if(getele){
+        getboxtitle.onmousedown = getmousedown;
+    }
 
     function getmousedown(e){
-        // console.log('i am working'); 
-        // console.log(e.target);
+        // console.log("i am working");
 
         getcx = e.clientX;
         getcy = e.clientY;
-
-        console.log(getcx,getcy);
+        // console.log(getcx,getcy);
 
         document.onmousemove = dragme;
+
         document.onmouseup = stopdragme;
 
-        console.log('step 1',getcx,getcy);
+        // console.log('step 1 ' , getcx,getcy);
 
-
+        // getbtns.classList.remove('show');
     }
 
     function dragme(e){
-    //    console.log(e.target);
+        // console.log(e.target);
+        
+        setcx = getcx - e.clientX;    //step 3
+        setcy = getcy - e.clientY;
+        // console.log(getcx,setcx);
+        // console.log(getcy,setcy);
 
-    //    console.log(getcx,getcy);
-      console.log('new position',e.clientX,e.clientY)
+        //result for new pin position
+        getcx = e.clientX;  //step 4 overwrite
+        getcy = e.clientY;
 
-    // ခုလက်ရှိ ရှိတဲ့နေရာကို သိဖို့ 
-       setcx = getcx - e.clientX;
-       setcy = getcy - e.clientY;
-        // ရွေ့လိုက်တဲ့ နေရာကို သိဖို့အတွက်  
- getcx = e.clientX;
- getcy = e.clientY;
- console.log('step 2',getcx,getcy);
+        console.log('step 2',getcx,getcy);
 
-    //    console.log(getcx,setcx);
-    //    console.log(getcy,setcy);
+        const btnleft = getele.offsetLeft;  //step 3
+        const btntop = getele.offsetTop;
 
-       const btnleft = getele.offsetLeft;
-       const btntop = getele.offsetTop;
-       console.log(btnleft,btntop)
+        // console.log(btnleft,btntop)
 
-       getele.style.left = (btnleft-setcx)+"px";
-       getele.style.top = (btntop-setcy)+"px";
+        getele.style.left = (btnleft-setcx) + "px";
+        getele.style.top = (btntop-setcy) + "px";
 
-    //    console.log(btnleft-setcx,btntop-setcy)
+        // console.log(btnleft-setcx,btntop-setcy);
+
+        getbtns.classList.remove('show');
     }
 
-    function stopdragme(e){
-        // console.log(e.target)
+    function stopdragme(){   //step 2
         document.onmousemove = null;
-        document.onmouseup = null
+        document.onmouseup = null;
     }
+
 }
 
-// လက်ရှိရှိတဲ့ နေရာကနေ သွားထားမယ့် နေရာကို နှုတ်ပေးရတာ
 
-// 8DG 
+function smallmenu(icobox) {
+     console.log(icobox);
+
+     if(icobox.classList.contains('btn-icon')){
+        console.log('yes')
+     }else{
+          console.log('no')
+     }
+}
+//8DG
+
+// 21MM 
+
+ 
