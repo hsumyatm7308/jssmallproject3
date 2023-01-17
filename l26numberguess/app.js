9278e0bd58decab47aaf88080a13b3c1c828dd07
EXY <107622230+hsumyatm7308@users.noreply.github.com>
2023-01-17 18:08:53 +0630

    1/17

--- l26numberguess/app.js
+++ l26numberguess/app.js
@@ -1,67 +1,117 @@
-// get ui 
-const minnum = document.querySelector('.minnumber'),
-maxnum = document.querySelector('.maxnumber');
+//Get UI
+const minnum = document.querySelector(".minnumber"),
+      maxnum = document.querySelector('.maxnumber'),
+      getinput = document.querySelector('#guessnumber'),
+      getbtn = document.querySelector("#btn"),
+      getmessage1 = document.querySelector('.message1'),
+      getmessage2 = document.querySelector('.message2'),
+      getgamecnt = document.getElementById('game-container'),
+      getmicbtn = document.getElementById('mic-btn'),
+      getvoccnt = document.getElementById('voice-container');
 
-const getinput = document.querySelector('#guessnumber');
-const getbtn = document.querySelector('#btn');
 
-const message1 = document.querySelector('.message1');
-const message2 = document.querySelector('.message2');
 
-let min = 1,
-max = 10,
-gameleft = 3,
-winningum = 5;
+let min = 10,
+    max = 100,
+    gameleft = 3,
+    winningnumber = 10;
 
-minnum.textContent = min;
-maxnum.textContent = max;
+    minnum.textContent = min;
+    maxnum.textContent = max;
 
 function randomnum(min,max){
-    let getrdm = Math.floor(Math.random()*(max-min)+1)
+    let getrdm = Math.random() * (max-min)+1;
     return getrdm;
 }
 
+// console.log(winningnumber)
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+let getrec = new window.SpeechRecognition;
+// console.log(getrec)
+
+getmicbtn.addEventListener('click',function(){
+//    For Chrome Support 
+// Start Recognition ,start() come frome recognition 
+    getrec.start();
+    getrec.addEventListener('result',(e)=>talking(e));
+})
+
+// object ta khu lone pyan kaw chin loh 
+
+function talking(ele){
+    console.log(ele);
+    const micresult = ele.results[0][0].transcript;
+    console.log(micresult);
+    micmessage(micresult);
+}
+
+function micmessage(msg){
+    getvoccnt.innerHTML = ` 
+    <span class="voicemessage"> Did you say!! ${msg}</span>
+    
+    `
+}
+
+function getnumber(msg){
+    const getnum = +msg;
+    console.log(typeof getnum);
+
+    getinput.value =getnum;
+}
+
 function setmessage1(msg,color){
-    message1.textContent = msg;
-    message1.style.color = color;
+    getmessage1.textContent = msg;
+    getmessage1.style.color = color;
 }
 
 function setmessage2(msg,color){
-    message2.textContent = msg;
-    message2.style.color = color;
+    getmessage2.textContent = msg;
+    getmessage2.style.color = color;
 }
 
 function gameover(won,msg){
     let color;
-    won === true ? color = 'green' : color = 'red';
 
+    won === true? color = "green" : color = "red";
     getinput.disabled = true;
+
     getinput.style.borderColor = color;
-
     setmessage1(msg,color);
-
-    getbtn.vlaue ='Play Again';
+    getbtn.value = "Play Again";
+    getbtn.classList.add('playagain');
 }
 
 getbtn.addEventListener('click',function(){
-    let guess = +getinput.vlaue;
+    let guess = +getinput.value;
+    // console.log(typeof guess)
 
-    console.log(guess);
     if(guess < min || guess > max || isNaN(guess)){
-        setmessage2(`Please enter a number between ${max} to ${min}`,'red');
+        setmessage2(`Please enter a number between ${min} to ${max}`,"red");
     }
 
-    if(guess === winningum){
-        // Game Over WON 
-        gameover(true,`${winningum} is correct.You Win`)
+    if(guess === winningnumber){
+        // Game Over Won
+        gameover(true,`${winningnumber} is correct!! You win`);
+        
     }else{
-        gameleft --;
+        gameleft--;
         if(gameleft === 0){
-            //   Game Over Lose
+            // Game Over Lose
+            gameover(false,`Game Over , You Lost , The correct Number is ${winningnumber}`);
         }else{
-              //Continue Gmae
+            //Continue Game
+            getinput.style.borderColor = "red";
+            getinput.value = "";
+            setmessage1(`${guess} is not correct , ${gameleft} guess left`,'blue');
         }
     }
-})
+});
 
-// 9VI 
+getgamecnt.addEventListener('mousedown',function(e){
+    console.log(e.target);
+    if(e.target.classList.contains('playagain')){
+        window.location.reload();
+    }
+});
+
+// 17RC 
